de2278adceb3a93c7a6988e2b82458935bb56d79
Harsharm <rmharsha2@gmail.com>
2020-02-06 11:50:59 +0530

    demogg

--- testproject/src/genriqe/Auto_constant.java
+++ testproject/src/genriqe/Auto_constant.java
@@ -3,6 +3,5 @@ package genriqe;
 public interface Auto_constant {
 	String gecko_kay="webdriver.gecko.driver";
 	String gecko_value="E:/manufactureing domine/testproject/drivers/geckodriver.exe";
-	String path="C:/Users/Harsha/git/TestRepositiryrr/testproject/excel/New Microsoft Excel Worksheet.xlsx";
 
 }
